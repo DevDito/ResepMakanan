d2ff1433d3f3a2d71d7b8e31262fcbd85a0870ec
Satria Amru Dito <itsdito0819@gmail.com>
2021-01-06 23:02:51 +0700

    Initial commit

---
+++ app/src/main/java/com/satriaamrudito/resepmakanan/model/ModelMain.kt
@@ -0,0 +1,15 @@
+package com.satriaamrudito.resepmakanan.model
+
+import java.io.Serializable
+
+class ModelMain : Serializable {
+
+    @JvmField
+    var strCategory: String? = null
+
+    @JvmField
+    var strCategoryThumb: String? = null
+
+    var strCategoryDescription: String? = null
+
+}
